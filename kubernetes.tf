01ae8f14f7c7330604612d42dd24452a8c06a50f
danielchoi158 <danielchoi158@gmail.com>
2020-06-20 21:25:30 +1000

    WEB-26 add kubernetes config

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,4 +1,13 @@
-provider "kubernetes" {}
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  load_config_file = false
+  host = google_container_cluster.primary.endpoint
+  token = data.google_client_config.default.access_token
+  client_certificate = base64decode(google_container_cluster.primary.master_auth[0].client_certificate)
+  client_key = base64decode(google_container_cluster.primary.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
 
 resource "kubernetes_deployment" "portfolio-frontend" {
   metadata {
