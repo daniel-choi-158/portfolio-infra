3912d431699bc5a0b21c8308613a5c849313ac06
daniel-choi-158 <danielchoi158@gmail.com>
2020-06-24 14:54:25 +1000

    update gke master engine

--- gke.tf
+++ gke.tf
@@ -1,3 +1,10 @@
+data "google_container_engine_versions" "australiasoutheast1a" {
+  provider       = "google-beta"
+  location       = var.zone
+  version_prefix = "1.15."
+}
+
+
 variable "gke_username" {
   default     = ""
   description = "gke username"
@@ -21,6 +28,7 @@ variable "zone" {
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.zone
+  master_version = data.google_container_engine_versions.australiasoutheast1a.latest_master_version
 
   remove_default_node_pool = true
   initial_node_count       = 1
